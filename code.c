f47e37a82fc2b62be52e22da9d8dd9610c695938
HaneenEhap <ehapelgawesh@gmail.com>
2026-04-15 21:56:10 +0200

    Add files via upload

---
+++ code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+const float pi=3.14;
+int r=7;
+float result1=2*pi*r;
+float result2=pi*r*r;
+printf(" circumference:%f",result1);
+printf("\n");
+printf(" Area of the circle:%f",result2);
+return 0;
+}
