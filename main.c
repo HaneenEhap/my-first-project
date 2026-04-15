23c956bb147cac6519cb88623ba47b1ed7bcb763
HaneenEhap <ehapelgawesh@gmail.com>
2026-04-15 18:28:44 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main (){
+ int x =8;
+ int y =4;
+ int result1=(x+y)*2;
+ int result2=(x*y);
+ printf("%d",result1);
+ printf ("\n");
+ printf ("%d",result2);
+ return 0;
+ }
